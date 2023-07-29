3a9d05d123a08ff0ae9431f67efbaa8594bdea1d
solomonbaez <solomon.james.baez@gmail.com>
2023-07-29 13:47:07 -0700

    Added naive database room management

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+
+    ## snapshot every instance
+    updated = models.DateField(auto_now=True)
+    ## snapshot initial instance
+    created = models.DateField(auto_now_add=True)
+
+    ## return db info
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
-
-rooms = [
-    {"id": 1, "name": "Django Practice!"},
-    {"id": 2, "name": "Frontend Practice!"},
-    {"id": 3, "name": "Backend Practice!"},
-]
+from .models import Room
 
 
 # function based views
 def home(request):
+    # object model manager
+    rooms = Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
-    # add database query
-    room = None
-    for r in rooms:
-        if r["id"] == int(pk):
-            room = r
+    room = Room.objects.get(id=pk)
 
     context = {"room": room}
     return render(request, "base/room.html", context)
