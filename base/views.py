11a47328f2ca7aa7c8cdef5349c4b34d0a71a9a0
solomonbaez <solomon.james.baez@gmail.com>
2023-07-29 11:55:05 -0700

    Added main.html template

--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # function based views
 def home(request):
-    return HttpResponse("Home Page")
+    return render(request, "home.html")
+
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request, "room.html")
