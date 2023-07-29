dea4b338af8d9612f2fc67fc77b08fc6c94858f9
solomonbaez <solomon.james.baez@gmail.com>
2023-07-29 12:43:21 -0700

    Light path restructuring

--- base/views.py
+++ base/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+rooms = [
+    {"id":1, "name":"Django Practice!"},
+    {"id":2, "name":"Frontend Practice!"},
+    {"id":3, "name":"Backend Practice!"},
+]
 
 # function based views
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
 def room(request):
-    return render(request, "room.html")
+    return render(request, "base/room.html")
