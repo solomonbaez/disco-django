55fed42d63d5a7dcba06ac1d670380f39b45912d
solomonbaez <solomon.james.baez@gmail.com>
2023-07-29 18:27:14 -0700

    Expanded models module -> Topic/Room/Message

--- base/models.py
+++ base/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+# one to many relation
 class Room(models.Model):
-    # host =
-    # topic =
-    name = models.CharField(max_length=150)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
 
     ## snapshot every instance
     updated = models.DateField(auto_now=True)
@@ -16,3 +24,16 @@ class Room(models.Model):
     ## return db info
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # relate a room to a message -> clear db on delete
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        # limit return
+        return self.body[0:50]
