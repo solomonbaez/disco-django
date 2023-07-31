c06183745ee54b2f2623f0dc111dddba1bbb9971
solomonbaez <solomon.james.baez@gmail.com>
2023-07-30 17:40:42 -0700

    Finished basic CRUD functionality

--- base/views.py
+++ base/views.py
@@ -36,6 +36,22 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = CreateRoomForm(instance=room)
+    if request.method == "POST":
+        # validate form data
+        form = CreateRoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
     context = {"form": form}
 
     return render(request, "base/room_form.html", context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect("home")
+
+    return render(request, "base/room_delete.html", {"obj": room})
