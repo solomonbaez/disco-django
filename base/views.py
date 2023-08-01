b890b03d62791c3fe335623bb3d2f7dd228e5078
solomonbaez <solomon.james.baez@gmail.com>
2023-08-01 11:17:12 -0700

    Added basic profile view and finished basic UI

--- base/views.py
+++ base/views.py
@@ -116,6 +116,21 @@ def room(request, pk):
     return render(request, "base/room.html", context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {
+        "user": user,
+        "rooms": rooms,
+        "topics": topics,
+        "room_messages": room_messages,
+        "recent_messages": room_messages,
+    }
+    return render(request, "base/profile.html", context)
+
+
 # restrict room creation by SID
 @login_required(login_url="login")
 def createRoom(request):
