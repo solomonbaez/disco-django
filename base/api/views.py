5c400c1e86c5f2b91c6535efa3b97c12c0141df1
solomonbaez <solomon.james.baez@gmail.com>
2023-08-02 15:14:28 -0700

    Finished basic RESTFUL API

--- base/api/views.py
+++ base/api/views.py
@@ -1,13 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from base.models import Room
+from .serializers import RoomSerializer
+
 
 # restrict requests
-@api_view(
-    [
-        "GET",
-    ]
-)
+@api_view(["GET"])
 def getRoutes(request):
     routes = [
         "GET /api",
@@ -16,3 +15,21 @@ def getRoutes(request):
     ]
     # restrict to python
     return Response(routes)
+
+
+@api_view(["GET"])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+
+    # push serialized data
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+
+    # push serialized data
+    return Response(serializer.data)
